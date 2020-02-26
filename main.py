581aff7dbbadf01c87a34e2158ad8f2c724135b3
ogasyunn <os132109@icloud.com>
2020-02-27 00:32:11 +0900

    start point

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, FollowEvent, TextMessage, TextSendMessage, ImageSendMessage, VideoSendMessage, StickerSendMessage, AudioSendMessage, TemplateSendMessage,
-    ConfirmTemplate, PostbackAction, MessageAction
+    *, MessageEvent, FollowEvent, TextMessage, TextSendMessage, ImageSendMessage, VideoSendMessage, StickerSendMessage, AudioSendMessage, TemplateSendMessage,
+    ConfirmTemplate, PostbackAction, MessageAction, 
 )
 from flask_sqlalchemy import SQLAlchemy
 
