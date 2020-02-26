5388e59bbfed47be5d4cc7c7314f28e9cd5cc5aa
ogasyunn <os132109@icloud.com>
2020-02-27 07:02:24 +0900

    kojinn setted

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ import os
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///local.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
