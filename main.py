42c0bd9405667b397a2f328419a69c70add09dbd
ogasyunn <os132109@icloud.com>
2020-02-26 19:34:38 +0900

    postgresssql

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, VideoSendMessage, StickerSendMessage, AudioSendMessage, TemplateSendMessage,
     ConfirmTemplate, PostbackAction, MessageAction
 )
-import os
+from flask_sqlalchemy import SQLAlchemy
 
+import os
+"""
 import pymysql
 import pymysql.cursors
- 
+"""
 connection = pymysql.connect(
         host='us-cdbr-iron-east-04.cleardb.net',
         user='bc9fde5ae25666:d6b61d67',
@@ -31,6 +33,9 @@ connection = pymysql.connect(
         connection.close()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
+
 
 #環境変数取得
 LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
@@ -56,11 +61,28 @@ def callback():
 
     return 'OK'
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.String(80), unique=True)
+    message = db.Column(db.String(80), unique=True)
 
+    def __init__(self, userid, message):
+        self.userid = userid
+        self.mwssage = message
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 
+    user = User(event.source.user_id, event,message.text)
+    db.session.add(User)
+    db.session.commit()
+
+    sendmessages = []
+
+    messages = db.session.query(User).all()
+    for message in messages:
+        sendmessages.append(TextSendMessage(message))
+"""
     confirm_template_message = TemplateSendMessage(
         alt_text='Confirm Template',
         template=ConfirmTemplate(
@@ -78,10 +100,10 @@ def handle_message(event):
             ]
         )
     )
-
+"""
     line_bot_api.reply_message(
         event.reply_token,
-        confirm_template_message
+        sendmessages
         )
 
 if __name__ == "__main__":
