7d14f9019cb56faf596d5751637b56cf8be206b2
ogasyunn <os132109@icloud.com>
2020-02-26 22:44:17 +0900

    start point

--- main.py
+++ main.py
@@ -13,25 +13,7 @@ from linebot.models import (
 from flask_sqlalchemy import SQLAlchemy
 
 import os
-"""
-import pymysql
-import pymysql.cursors
 
-connection = pymysql.connect(
-        host='us-cdbr-iron-east-04.cleardb.net',
-        user='bc9fde5ae25666:d6b61d67',
-        password='d6b61d67',
-        db='mysql',
-        charset='utf8',
-        cursorclass=pymysql.cursors.DictCursor)
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute(sql)
-            results = cursor.fetchall()                
-    finally:
-        cursor.close()
-        connection.close()
-"""
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
@@ -44,14 +26,23 @@ LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
-class User(db.Model):
+class Instruments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    groupid = db.Column(db.string(80))
     userid = db.Column(db.String(80))
-    message = db.Column(db.String(80))
+    message = db.Column(db.String(100))
 
-    def __init__(self, userid, message):
+    def __init__(self, groupid, userid, message):
+        self.groupid = groupid
         self.userid = userid
         self.message = message
+
+class Answer(dbModel):
+    id = db.column(db.Integer, primary_key=True)
+    answer = db.Column(db.String(80))
+
+    def __init__(self, answer):
+        self.answer = answer
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -69,50 +60,19 @@ def callback():
         abort(400)
 
     return 'OK'
-"""
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    userid = db.Column(db.String(80), unique=True)
-    message = db.Column(db.String(80), unique=True)
 
-    def __init__(self, userid, message):
-        self.userid = userid
-        self.mwssage = message
-"""
+@hanndler.add(FollowEvent)
+def follow_event(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text="こんにちは！")
+        )
+
+def register():
+    groupid = 
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-
-    user = User(event.source.user_id, event.message.text)
-    db.session.add(user)
-    db.session.commit()
-
-    sendmessages = []
-
-    messages = db.session.query(User).all()
-    for message in messages:
-        sendmessages.append(TextSendMessage(message.message))
-    line_bot_api.reply_message(event.reply_token,sendmessages[-5:])
-"""
-    confirm_template_message = TemplateSendMessage(
-        alt_text='Confirm Template',
-        template=ConfirmTemplate(
-            text='are you ok?',
-            actions=[
-                PostbackAction(
-                    label='postback',
-                    display_text='postback text',
-                    data='action=buy&itemid=1'
-                ),
-                MessageAction(
-                    label='message',
-                    text='I understood'
-                )
-            ]
-        )
-    )
-"""
-
-   ## line_bot_api.reply_message(event.reply_token,sendmessages)
 
 if __name__ == "__main__":
 #    app.run()
