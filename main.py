9539f480ac83911084f160f0eb422b9cbde2f9b0
ogasyunn <os132109@icloud.com>
2020-02-27 00:50:23 +0900

    start point

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ def follow_event(event):
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    profile = line_bot_api.get_profile(event.source.userId)
+    profile = line_bot_api.get_profile(event.source.user_id())
     sendmessage = dealmessage(event.message.text, profile)
     line_bot_api.reply_message(
         event.reply_token,
